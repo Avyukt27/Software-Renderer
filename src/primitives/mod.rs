b4919cf85e7fc0b4f0d0eed42c4a2e5ebdfcce57
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-12-29 16:59:24 +0530

    Added normalize() for vertex struct for lighting

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -1,3 +1,4 @@
 pub mod colour;
+pub mod light;
 pub mod triangle;
 pub mod vertex;
--- src/primitives/vertex.rs
+++ src/primitives/vertex.rs
@@ -9,4 +9,12 @@ impl Vertex {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
     }
+
+    pub fn normalize(&self) -> Self {
+        let length = (self.x * self.x + self.y * self.y + self.z * self.z).sqrt();
+        if length == 0.0 {
+            return Self::new(0.0, 0.0, 0.0);
+        }
+        Self::new(self.x / length, self.y / length, self.z / length)
+    }
 }
