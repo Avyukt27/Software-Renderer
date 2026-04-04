fd1449684d373d5a88ae9c44b70094ea33defc4b
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-04-04 19:18:50 +0530

    "Initial" commit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-use winit::event_loop::EventLoop;
-
-mod app;
-mod camera;
-mod loader;
-mod math;
-mod mesh;
-mod primitives;
-mod renderer;
-
-use app::App;
-
 fn main() {
-    let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
-
-    let mut app = App::new(600, 600);
-    event_loop.run_app(&mut app).unwrap();
+    println!("Hello, world!");
 }
--- src/primitives/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod colour;
-pub mod glb_format;
-pub mod material;
-pub mod texture;
-pub mod triangle;
-pub mod vector;
-pub mod vertex;
