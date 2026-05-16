d85985ef9d7062f0d310ceb51df680eaf468e3ea
Avyukt27 <avyukt.aggarwal007@gmail.com>
2026-05-16 13:55:51 +0530

    Added vertex uv coords

--- src/loaders/obj.rs
+++ src/loaders/obj.rs
@@ -15,6 +15,7 @@ pub struct Material {
 
 struct ObjIndex {
     v_idx: usize,
+    vt_idx: usize,
     vn_idx: usize,
     mat_name: String,
 }
@@ -104,6 +105,7 @@ pub fn load_obj(path_str: &str) -> (Vec<Vertex>, Vec<u16>) {
 
     let mut raw_positions = Vec::new();
     let mut raw_normals = Vec::new();
+    let mut raw_uvs = Vec::new();
     let mut faces = Vec::new();
 
     let mut materials: HashMap<String, Material> = HashMap::new();
@@ -117,35 +119,33 @@ pub fn load_obj(path_str: &str) -> (Vec<Vertex>, Vec<u16>) {
         }
 
         match tokens[0] {
-            "mtllib" => {
-                let mtl_filename = tokens[1];
-                let mtl_path = base_dir.join(mtl_filename);
-                materials = load_mtl(&mtl_path);
-            }
-            "usemtl" => {
-                current_material_name = tokens[1].to_string();
-            }
-            "v" => {
-                let x: f32 = tokens[1].parse().unwrap();
-                let y: f32 = tokens[2].parse().unwrap();
-                let z: f32 = tokens[3].parse().unwrap();
-                raw_positions.push(glam::Vec3::new(x, y, z));
-            }
-            "vn" => {
-                let x: f32 = tokens[1].parse().unwrap();
-                let y: f32 = tokens[2].parse().unwrap();
-                let z: f32 = tokens[3].parse().unwrap();
-                raw_normals.push(glam::Vec3::new(x, y, z));
-            }
+            "mtllib" => materials.extend(load_mtl(&base_dir.join(tokens[1]))),
+            "usemtl" => current_material_name = tokens[1].to_string(),
+            "v" => raw_positions.push(glam::Vec3::new(
+                tokens[1].parse().unwrap(),
+                tokens[2].parse().unwrap(),
+                tokens[3].parse().unwrap(),
+            )),
+            "vn" => raw_normals.push(glam::Vec3::new(
+                tokens[1].parse().unwrap(),
+                tokens[2].parse().unwrap(),
+                tokens[3].parse().unwrap(),
+            )),
+            "vt" => raw_uvs.push(glam::Vec2::new(
+                tokens[1].parse().unwrap(),
+                1.0 - tokens[2].parse::<f32>().unwrap(),
+            )),
             "f" => {
                 for i in 1..=3 {
                     let parts: Vec<&str> = tokens[i].split('/').collect();
 
                     let v_idx = parts[0].parse::<usize>().unwrap() - 1;
+                    let vt_idx = parts[1].parse::<usize>().unwrap() - 1;
                     let vn_idx = parts[2].parse::<usize>().unwrap() - 1;
 
                     faces.push(ObjIndex {
                         v_idx,
+                        vt_idx,
                         vn_idx,
                         mat_name: current_material_name.clone(),
                     });
@@ -161,12 +161,23 @@ pub fn load_obj(path_str: &str) -> (Vec<Vertex>, Vec<u16>) {
     let mut vertex_cache = HashMap::new();
 
     for corner in faces {
-        let cache_key = (corner.v_idx, corner.vn_idx, corner.mat_name.clone());
+        let cache_key = (
+            corner.v_idx,
+            corner.vt_idx,
+            corner.vn_idx,
+            corner.mat_name.clone(),
+        );
 
         if let Some(&existing_index) = vertex_cache.get(&cache_key) {
             out_indices.push(existing_index);
         } else {
             let position = raw_positions[corner.v_idx];
+
+            let uv = if corner.vt_idx < raw_uvs.len() {
+                raw_uvs[corner.vt_idx].to_array()
+            } else {
+                [0.0, 0.0]
+            };
 
             let diffuse_color = if let Some(mat) = materials.get(&corner.mat_name) {
                 [mat.diffuse[0], mat.diffuse[1], mat.diffuse[2], 1.0]
@@ -177,6 +188,7 @@ pub fn load_obj(path_str: &str) -> (Vec<Vertex>, Vec<u16>) {
             let vertex = Vertex {
                 position: position.to_array(),
                 colour: diffuse_color,
+                uv,
             };
 
             let new_index = out_vertices.len() as u16;
